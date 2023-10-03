7f625795eb1478f812ab38db97d5d67d384975f7
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2023-10-03 10:06:41 +0530

    useState() about to complete.

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people,setPeople] = useState(data);
+
+  const MapPeople = ()=>{
+      return people.map((person)=>{
+         const {id,name} = person;
+         return (
+              <div key={id}>
+              <h1>{name}</h1>
+              <button onClick={()=>removeOne(id)}>Remove me</button>
+              </div>
+         )
+      })
+  }
+  
+  const removeOne = (id)=>{
+    console.log(`${data[id-1].name} was removed.`); 
+    const newPeople = people.filter((person)=>{
+     return person.id != id;
+    })
+    setPeople(newPeople);
+  }
+  const removeAll = ()=>{
+    setPeople([]);
+  }
+  
+  return (
+    <div>
+      <MapPeople />
+      <button className = "btn" onClick={removeAll} style={{marginTop:'2rem'}}>Remove All</button>
+    </div>
+  )
 };
 
 export default UseStateArray;
--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,27 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  
+  const [name,setName] = useState('Himanshu')
+  const [age,setAge] = useState(22)
+  const [hobby,setHobby] = useState('Sleeps all day')
+
+  const updatePerson = ()=>{
+      setName("Rohan")
+      setAge(16)
+      setHobby("Sleeps All day.")
+  }
+
+  return (
+    <>
+    <h2>useState object example</h2>
+    <h3>{name}</h3>
+    <h4>{age}</h4>
+    <p>{hobby}</p>
+    <button className='btn' onClick={updatePerson}>New Person</button>
+    </>
+  )
+
 };
 
 export default UseStateObject;
